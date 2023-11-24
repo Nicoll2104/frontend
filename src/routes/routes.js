58409088aceb5220e67cc25f29ae79ee0406dd43
Nicoll2104 <soda210461@gmail.com>
2023-11-24 16:57:26 -0500

    actual

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,10 +1,12 @@
 import home from "../components/home.vue";
 import menu from "../components/menu.vue";
+import area from "../components/area.vue";
 import { createRouter, createWebHashHistory } from "vue-router";
 
 const routes = [
   { path: "/home", component: home},
   { path: "/menu", component: menu },
+  { path: "/area", component: area},
 ];
 
 export const router = createRouter({
