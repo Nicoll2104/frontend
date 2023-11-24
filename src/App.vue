3aedc8a89a7ccedd3b3cd994fd0dfdb149c01051
Nicoll2104 <soda210461@gmail.com>
2023-11-24 16:11:59 -0500

    actual

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,26 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<template>
+  <div class="q-pa-md ">
+    <router-view></router-view>
+  </div>
+</template>
+  
+
+
+<script>
+
+
+
 </script>
 
-<template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
+
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.q-pa-md {
+  background-image: url("./assets/4931029.jpg");
+  background-size: cover;
+  padding: 0%;
 }
 </style>
+
+
+
--- src/main.js
+++ src/main.js
@@ -1,5 +1,33 @@
 import { createApp } from 'vue'
-import './style.css'
+import { createPinia } from 'pinia';
+import { Quasar, Notify, Loading } from 'quasar';
 import App from './App.vue'
+import { router } from "./routes/routes.js";
+import 'quasar/src/css/index.sass';
+import axios from 'axios';
+import '@quasar/extras/material-icons/material-icons.css'
 
-createApp(App).mount('#app')
+const pinia = createPinia()
+const app = createApp(App)
+
+app.use(Quasar, {
+    plugins: {
+        Notify,
+        Loading
+    },
+    config: {
+        notify: {
+            position: 'top-right',
+            timeout: 2000,
+            color: 'primary',
+            textColor: 'white',
+        },
+        loading: {
+            position: 'center'
+        }
+    }
+});
+
+app.use(router)
+app.mount('#app')
+app.use(pinia)
