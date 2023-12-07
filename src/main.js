bc5d193353facd6ee02c6b451bddf9d347f84360
unknown <johanblancocata@gmail.com>
2023-12-07 15:53:34 -0500

    notificaciones

--- src/main.js
+++ src/main.js
@@ -1,21 +1,28 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia';
-import "./style.css";
-import { Quasar } from 'quasar';
 import App from './App.vue'
+import { createApp } from 'vue'
+import "./style.css";
+import { createPinia } from 'pinia';
 import { router } from "./routes/routes.js";
+import { Quasar, Dialog, Notify  } from 'quasar';
+import '@quasar/extras/material-icons/material-icons.css'
 import 'quasar/src/css/index.sass';
 import axios from 'axios';
-import '@quasar/extras/material-icons/material-icons.css'
+
+axios.defaults.baseURL = "http://localhost:3000/api/";
+//axios.defaults.baseURL = "https://transporte-el2a.onrender.com/api/";
 
 const pinia = createPinia()
 const app = createApp(App)
 
+app.use(router);
+app.use(pinia);
 app.use(Quasar, {
-    plugins: {
-    },
-});
+    plugins: { Dialog, Notify },
+    config: {
+      notify: {}/* look at QuasarConfOptions from the API card */
+    }
+  });
 
-app.use(router)
+
 app.mount('#app')
-app.use(pinia)
+
