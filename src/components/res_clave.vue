f6201b1b73ecf337bf087d9a11b05d123de8bc81
Karol <124207015+Karollll@users.noreply.github.com>
2024-02-16 13:59:48 -0500

    2

--- src/components/res_clave.vue
+++ src/components/res_clave.vue
@@ -1,92 +1,110 @@
 <template>
-    <div class="cont">
-      <div class="olascont">
-        <img class="olaazul" src="../assets/olaazul.svg">
-        <img class="olaverde" src="../assets/olaverde.svg">
-      </div>
-      <div class="opcioncont">
-        <p class="tittle">Restablecer contraseña</p>
-        <p class="subtittle">Por favor ingrese una nueva contraseña</p>
-  
-        <input class="input opcion" type="text" placeholder="Nueva contraseña">
-        <div class="contrasenacont">
-          <input class="input opcion" type="text" placeholder="Confirmar contraseña">
-        </div>
-        <router-link to="/" class="ingresarcont">
-          <button class="ingresar opcion">Cambiar</button>
-        </router-link>
-      </div>
+  <div class="cont">
+    <div class="olascont">
+      <img class="olaazul" src="../assets/olaazul.svg">
+      <img class="olaverde" src="../assets/olaverde.svg">
     </div>
+    <div class="q-pa-md row items-start q-gutter-md">
+      <q-card class="my-card">
+        <q-card-section>
+          <q-div class="text-h3">Restablecer Contraseña</q-div>
+          <p class="subtittle">Por favor ingrese una nueva contraseña</p>
+        </q-card-section>
+        <q-card-section style="max-width: 500px">
+          <q-input standout v-model="data.contrasena" label="Nueva Contraseña" />
+        </q-card-section>
+        <q-card-section style="max-width: 500px">
+          <q-input standout v-model="data.contrasena2" label="Confirmar Contraseña" />
+        </q-card-section>
+        <q-card-section style="max-width: 500px">
+          <q-btn push color="primary" label="RESTABLECER" @click="cambiar" :loading="loading"/>
+        </q-card-section>
+      </q-card>
+    </div>
+  </div>
 </template>
-<!-- <script setup>
-import { ref } from "vue";
-import { useLoginStore } from "../stores/login.js"
-import { useQuasar } from 'quasar'
-const loginStore = useLoginStore()
+<script setup>
+  import { ref } from "vue";
+  import { useQuasar } from 'quasar'
+  import { useRouter } from "vue-router";
+  import { useUsuarioStore } from "../stores/usuario.js"
+  import Cookies from 'js-cookie'
 
-const username = ref('');
-const password = ref('');
-const $q = useQuasar();
-let errorMessage = ref("");
-  
-const showDefault = () => {
-notification = $q.notify({
-    spinner: true,
-    message: 'Please wait...',
-    timeout: 0 
-});
-};
+  const UsuarioStore = useUsuarioStore()
+  const router = useRouter()
 
-let validacion = ref(false);
-let notification = ref(null);
-async function validar() {
-    if (!username.value && !password.value) {
-        errorMessage.value = "* Ingrese el usuario y la contraseña"
-    }else if(!username.value){
-        errorMessage.value = "* Ingrese el usuario"
-    }else if(!password.value){
-        errorMessage.value = "* Ingrese la contraseña"
-    }else{
-        errorMessage.value =""
-        validacion.value = true;
-};
-    if (validacion.value==true) {
-        try {
-          showDefault()
-          const res = await loginStore.Login({
-              cuenta: username.value,
-              clave: password.value
-          });
-          console.log(res);
-          localStorage.setItem('token', res.data.token)
-          if(notification) {
-              notification()
-          }
-          $q.notify({
-              spinner: false, 
-              message: "Ingresado al programa", 
-              timeout: 2000,
-              type: 'positive',
-          });
-      } catch (error) {
-          if(notification) {
-              notification()
-          };
-          $q.notify({
-              spinner: false, 
-              message: `${error.response.data.msg}`, 
-              timeout: 2000,
-              type: 'negative',
-          });
-      };
-      }
-      
-      validacion.value = false  
+  const data = ref({
+    contrasena: "",
+    contrasena2: "",
+  });
+
+  const $q = useQuasar();
+  let errorMessage = ref("");
+
+  const showDefault = () => {
+    notification = $q.notify({
+      spinner: true,
+      message: 'Please wait...',
+      timeout: 0
+    });
   };
-  </script> -->
-  
-  <!-- scoped sirve para evitar que los estilos afecte a los
-  demas componentes  -->
+
+  let validacion = ref(false);
+  let notification = ref(null);
+  let loading = ref(false)
+
+  function cambiar() {
+    const arrData = Object.entries(data.value)
+    console.log(arrData);
+    for (const d of arrData) {
+      console.log(d);
+      if (d[1] === null) {
+        notificar('negative', 'Por favor complete todos los campos')
+        return
+      }
+      if(typeof d[1] === 'string'){if (d[1].trim() === "") {
+        console.log("h");
+        notificar('negative','Por favor complete todos los campos')
+        return
+      }}
+    }
+
+    if(data.value.contrasena !== data.value.contrasena2){
+      notificar('negative', 'Las contraseñas no coinciden')
+      return
+    }
+
+    restablecerContrasena()
+  }
+
+  async function restablecerContrasena() {
+    try {
+      console.log("Esperando confirmación...");
+      loading.value = true;
+      const response = await UsuarioStore.restablecerContrasena(data.value);
+      console.log(response);
+
+      if(!response) return
+
+      if (response.status != 200) {
+        notificar('negative', response.mensaje)
+        return;
+      }
+      notificar('positive', 'Sección exitosa')
+      router.push("/home");
+    } catch (error) {
+      console.log(error);
+  }
+}
+function notificar(tipo, msg) {
+  $q.notify({
+    type: tipo,
+    message: msg,
+    position: "top"
+  })
+}
+    </script>
+
   <style scoped>
   .paleta {
     background-color:
@@ -97,79 +115,43 @@ async function validar() {
     margin: 0;
     padding: 0;
   }
-  
-  
-  
-  
-  .tittle {
-    margin-top: 5px;
-    font-size: 100px;
-    font-weight: 700;
-    color: #3F497F;
-  }
-  
-  
-  
-  .subtittle {
+  .my-card{
+    background-color: accent;
+    transform: translate(0px, -50px);
+}
+
+.cont {
+    display: flex;
+    justify-content: center;
+    height: 100vh;
+    align-items: center;
+}
+
+.olascont {
+    flex: 1;
+}
+.subtittle {
     font-size: 20px;
     font-weight: bold;
     color: #3F497F;
     margin-bottom: 10px;
-  }
-  
-  
-  
-  .opcion {
-    width: 95vw;
-    max-width: 525px;
-  }
-  
-  .input {
-    background-color: #29A19C;
-    color: white;
-    border: none;
-    border-radius: 10px;
-    font-size: 20px;
-    text-align: left;
-    padding: 15px 30px;
-    margin-top: 70px;
-  }
-  
-  
-  
-  input::placeholder {
+}
+
+
+input::placeholder {
     color: rgb(255, 255, 255);
-  }
-  
-  .input:focus {
-    outline: solid #A3F7BF;
-  }
-  
-  .contrasenacont {
-    width: min-content;
-    margin: auto;
-  }
-  
-  .contrasenaayuda {
+}
+
+
+.contrasenaayuda {
     text-align: right;
     color: #29A19C;
     font-weight: 700;
     text-decoration-line: underline;
     margin: 10px 0px;
-  }
-  
-  
-  .ingresar {
-    font-size: 30px;
-    font-weight: 700;
-    padding: 10px 100px;
-    background-color: #3F497F;
-    color: white;
-    border-radius: 20px;
-    margin: 50px 0px;
-  }
-  
-  .olascont {
+}
+
+.olascont {
     user-select: none;
     z-index: -1;
     Overflow: hidden;
@@ -179,60 +161,35 @@ async function validar() {
     bottom: 0;
     right: 50%;
     transform: translate(50%, 0);
-  }
-  
-  .olaverde,.olaazul {
+}
+
+@media only screen and (min-width: 1200px) {
+    .olascont{
+        bottom: -2vw;
+    }
+}
+
+.olaverde,
+.olaazul {
     position: absolute;
     width: 100vw;
-    min-width: 1280px;
+    min-width: 980px;
     bottom: 0;
     right: 50%;
     transform: translate(50%, 0);
-  }
-  
-  
-  @media screen and (min-height: 850px ) {
-    .opcioncont {
-      margin-top: 5vh;
-    }
-  }
-  
-  @media screen and (max-width: 640px ) {
-    .tittle {
-    font-size: 80px;
-    font-weight: 700;
-  }
-  }
-  
-  @media screen and (max-width: 500px ) {
-    .tittle {
-    font-size: 55px;
-    font-weight: 700;
-    }
-  
-    .subtittle,.input,.contrasenaayuda {
-      font-size: 17px;
-    }
-  
-    .ingresar{
-      font-size: 20px;
-      }
-  }
-  .cont {
-  display: flex;
-  justify-content: center;
-
-  height: 100vh; 
-}
-
-.olascont {
-  flex: 1;
-}
-
-.opcioncont {
-  flex: 1;
-  text-align: center; 
 }
 
 
+.contrasenaayuda {
+    background-color: transparent;
+    border: none;
+    color: #29A19C;
+    text-decoration: underline;
+    cursor: pointer;
+    font-size: 14px;
+}
+
+.contrasenaayuda:hover {
+    color: #3F497F;
+}
   </style>
